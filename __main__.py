2723b694c28160bcd73fd7d09847120059709c9d
come_ill_foo <rayveleaveme34@inbox.ru>
2024-06-30 21:40:31 +0100

    refactor: remove useless code and do at temporary directories

--- __main__.py
+++ __main__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-import click, os, shutil, enum, errno, re, subprocess as sp
-from typing import Tuple, Optional
+import os
+from typing import Tuple
+import errno
+import shutil
+import click
+import tempfile
+
+from wrappers import _patch, _revert, _diff, PatchResult, PATCH_ERROR_REASONS
+
 
 class TextStyle:
    PURPLE = '\033[95m'
@@ -15,35 +22,10 @@ class TextStyle:
    END = '\033[0m'
 
 
-class PatchResult(enum.Enum):
-    OK = 0
-    REVERSE_APPLIED = 1
-    HUNK_SUCCEED = 2
-    HUNK_FAILED = 3
-    INVALID_FORMAT = 4
-    FILE_NOT_FOUND = 5
-    EOF = 6
-    ERROR = 7
-
-    def is_ok(self) -> bool:
-        return self == PatchResult.OK or self == PatchResult.HUNK_SUCCEED
-
-
-PATCH_ERROR_REASONS = {
-    PatchResult.REVERSE_APPLIED: 'Already applied',
-    PatchResult.HUNK_FAILED: 'Hunk failed',
-    PatchResult.INVALID_FORMAT: 'Invalid format',
-    PatchResult.FILE_NOT_FOUND: 'Can\'t find file to patch',
-    PatchResult.EOF: 'Unexpected end of patch',
-    PatchResult.ERROR: 'Error',
-}
-
-
-VERBOSE = False
-
 OK = f'{TextStyle.BOLD}{TextStyle.GREEN}\u2713{TextStyle.END}'
 NOTOK = f'{TextStyle.BOLD}{TextStyle.RED}\u2717{TextStyle.END}'
 WARN = f'{TextStyle.BOLD}{TextStyle.YELLOW}\u26a0{TextStyle.END}'
+VERBOSE = False
 
 
 def _echo(text: str):
@@ -59,58 +41,32 @@ def _print_result(patch: str, rc: PatchResult = PatchResult.ERROR):
         click.echo(f'{NOTOK} {patch} ({PATCH_ERROR_REASONS[rc]})')
 
 
-
-def _validate_directory(ctx, param, value):
-    if not os.path.isdir(value):
-        _echo(f'Found file {value}, but expect directory')
-        value = os.path.abspath(os.path.join(value, os.pardir))
-        _echo(f'Forced to parent directory {value}')
-    return value
+def _redeploy(src: str, dst: str):
+    shutil.rmtree(dst)
+    shutil.copytree(src, dst)
 
 
-def _diff(a: str, b: str, extra_args: list[str]) -> Optional[str]:
-    cmd = ['diff', '-updrN', *extra_args, a, b]
-    _echo(' '.join([cmd[0].upper()] + cmd[1:]))
-    p = sp.Popen(cmd,
-                 stdout=sp.PIPE, stderr=sp.PIPE, text=True)
-    stdout, stderr = p.communicate()
-    _echo(f'stdout: {stdout.strip()}')
-    _echo(f'stderr: {stderr.strip()}')
-    return stdout if p.returncode != 2 else None
+def init_dirs(tmpdir, source) -> Tuple[str, str]:
+    def _make_pathes(dir: str) -> Tuple[str, str]:
+        return ('a', 'b') if dir == '.' else (os.path.join(dir, 'a'), os.path.join(dir, 'b'))
+
+    def _isdirs_or_die(*directories):
+        for directory in directories:
+            if not os.path.isdir(directory):
+                click.echo(f'{directory} not found')
+                exit(errno.ENOENT)
+            _echo(f'{directory} exists')
+
+    dira, dirb = _make_pathes(tmpdir)
+    shutil.copytree(source, dira)
+    shutil.copytree(dira, dirb)
+    _isdirs_or_die(dira, dirb)
+    return dira, dirb
 
 
-def _patch(target: str, patch: str, extra_args: list[str]) -> PatchResult:
-    cmd = ['patch', '-p1', '-F0', *extra_args, target]
-    _echo(' '.join([cmd[0].upper()] + cmd[1:]))
-    p = sp.Popen(cmd,
-                 stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-    stdout, stderr = '', ''
-    with open(patch, 'rb') as f:
-        stdout, stderr = p.communicate(input=f.read())
-        stdout, stderr = stdout.decode(), stderr.decode()
-    _echo(f'stdout: {stdout.strip()}')
-    _echo(f'stderr: {stderr.strip()}')
-    if p.returncode == 0:
-        if re.search('Hunk\s*#\d+\s*succeeded', stdout) is not None:
-            return PatchResult.HUNK_SUCCEED
-        return PatchResult.OK
-
-    if 'patch: ****' in stderr:
-        is_gibberish = 'Only garbage was found in the patch input.' in stderr
-        is_malformed = 'malformed patch at' in stderr
-        if is_gibberish or is_malformed:
-            return PatchResult.INVALID_FORMAT
-        elif 'unexpected end of file in patch' in stderr:
-            return PatchResult.EOF
-        return PatchResult.ERROR
-
-    is_partly_succeeded = re.search('(patching\s*.*\n(Hunk\s*#\d+\s*succeeded.*\n)?){2,}', stdout) is not None
-    is_hunk_failed = re.search('Hunk\s*#\d+\s*FAILED', stdout) is not None
-    if 'Assume -R' in stdout:
-        return PatchResult.HUNK_FAILED if is_partly_succeeded else PatchResult.REVERSE_APPLIED
-    elif 'can\'t find file to patch' in stdout:
-        return PatchResult.HUNK_FAILED if is_partly_succeeded else PatchResult.FILE_NOT_FOUND
-    return PatchResult.HUNK_FAILED if is_hunk_failed or is_partly_succeeded else PatchResult.ERROR
+def at_tempdir(callback) -> int:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        return callback(tmpdir)
 
 
 @click.group()
@@ -122,66 +78,68 @@ def cli(verbose: bool):
 
 @cli.command()
 @click.option('-C', '--directory', default=os.getcwd(),
-              type=click.Path(exists=True),
-              callback=_validate_directory,
-              help='Path where to put the compared directories')
-@click.argument('project_path', type=click.Path(exists=True))
-def deploy(directory: str, project_path: str):
-    """Replicates project to folders that are used for testing patches."""
-    dira = os.path.join(directory, 'a')
-    shutil.copytree(project_path, dira)
-    _echo(f'copied {project_path} to {dira}')
-    shutil.copytree(dira, os.path.join(directory, 'b'))
-    _echo(f'replicated {dira} to b')
-    click.echo(f'{project_path} successfully deployed')
-
-
-def _redeploy(src: str, dst: str):
-    shutil.rmtree(dst)
-    shutil.copytree(src, dst)
-
-def _make_pathes(dir: str) -> Tuple[str, str]:
-    return ('a', 'b') if dir == '.' else (os.path.join(dir, 'a'), os.path.join(dir, 'b'))
-
-
-def _isdirs_or_die(*directories):
-    for directory in directories:
-        if not os.path.isdir(directory):
-            click.echo(f'{directory} not found')
-            exit(errno.ENOENT)
-        _echo(f'{directory} exists')
-
-
-@cli.command()
-@click.option('-C', '--directory', default=os.getcwd(),
-              type=click.Path(exists=True),
-              callback=_validate_directory,
+              type=click.Path(exists=True, file_okay=False, dir_okay=True),
               help='Path where compared directories (a, b)')
 @click.argument('patches', nargs=-1,
                 type=click.Path(exists=True))
 def apply(directory: str, patches):
     """Tries to apply patches."""
-    fails = 0
-    dira, dirb = _make_pathes(directory)
-    _isdirs_or_die(dira, dirb)
+    def do_apply(tmpdir) -> int:
+        fails = 0
+        dira, dirb = init_dirs(tmpdir, directory)
 
-    for patch in patches:
-        rc = _patch(dirb, patch, ['-d'])
-        _print_result(patch, rc)
-        if not rc.is_ok():
-            fails += 1
-            _redeploy(dira, dirb)
-    exit(fails)
+        for patch in patches:
+            rc = _patch(_echo, dirb, patch, ['-d'])
+            _print_result(patch, rc)
+            if not rc.is_ok():
+                fails += 1
+                _redeploy(dira, dirb)
+        return fails
 
-
-def _revert(target: str, patch: str, extra_args: list[str]) -> PatchResult:
-    return _patch(target, patch, ['-R', *extra_args])
+    exit(at_tempdir(do_apply))
 
 
 @cli.command()
 @click.option('-C', '--directory', default=os.getcwd(),
-              type=click.Path(exists=True),
-              callback=_validate_directory,
+              type=click.Path(exists=True, file_okay=False, dir_okay=True),
+              help='Path where compared directories (a, b)')
+@click.argument('patches', nargs=-1,
+                type=click.Path(exists=True))
+def dehunk(directory: str, patches):
+    """Excludes succesful hunks in patches."""
+    def do_dehunk(tmpdir) -> int:
+        fails = 0
+        dira, dirb = init_dirs(tmpdir, directory)
+        applied = []
+
+        for patch in patches:
+            rc = _patch(_echo, dirb, patch, ['-d'])
+            _print_result(patch, rc)
+            if rc == PatchResult.HUNK_SUCCEED:
+                # with hunks to .orig
+                shutil.copyfile(patch, patch + '.orig')
+                dehunked = _diff(_echo, dira, dirb, ['-x', '*.orig'])
+                if dehunked is None:
+                    click.echo(f'Failed to dehunk {patch}')
+                    exit(fails)
+                with open(patch, 'w') as p:
+                    p.write(dehunked)
+                applied.append(patch)
+            elif rc == PatchResult.OK:
+                applied.append(patch)
+                _echo(f'no hunks for {patch} -- SKIP')
+            else:
+                fails += 1
+                applied = []
+        _redeploy(dira, dirb)
+        return fails
+
+    exit(at_tempdir(do_dehunk))
+
+
+@cli.command()
+@click.option('-C', '--directory', default=os.getcwd(),
+              type=click.Path(exists=True, file_okay=False, dir_okay=True),
               help='Patched directory')
 @click.argument('patches', nargs=-1,
                 type=click.Path(exists=True))
@@ -193,38 +151,6 @@ def revert(directory: str, patches):
         _print_result(patch, rc)
         if not rc.is_ok():
             exit(1)
-
-
-@cli.command()
-@click.option('-C', '--directory', default=os.getcwd(),
-              type=click.Path(exists=True),
-              callback=_validate_directory,
-              help='Path where compared directories (a, b)')
-@click.argument('patches', nargs=-1,
-                type=click.Path(exists=True))
-def dehunk(directory: str, patches):
-    """Excludes succesful hunks in patches."""
-    fails = 0
-    dira, dirb = _make_pathes(directory)
-    _isdirs_or_die(dira, dirb)
-    for patch in patches:
-        rc = _patch(dirb, patch, ['-d'])
-        _print_result(patch, rc)
-        if rc == PatchResult.HUNK_SUCCEED:
-            # with hunks to .old
-            shutil.copyfile(patch, patch + '.orig')
-            dehunked = _diff(dira, dirb, ['-x', '*.orig'])
-            if dehunked is None:
-                click.echo(f'Failed to dehunk {patch}')
-                exit(fails)
-            with open(patch, 'w') as p:
-                p.write(dehunked)
-        elif rc == PatchResult.OK:
-            _echo(f'no hunks for {patch} -- SKIP')
-        else:
-            fails += 1
-    _redeploy(dira, dirb)
-    exit(fails)
 
 
 if __name__ == '__main__':
---
+++ wrappers.py
@@ -0,0 +1,77 @@
+import enum
+import re
+from typing import Optional, Callable
+import subprocess as sp
+
+Logger = Callable[[str], None]
+
+class PatchResult(enum.Enum):
+    OK = 0
+    REVERSE_APPLIED = 1
+    HUNK_SUCCEED = 2
+    HUNK_FAILED = 3
+    INVALID_FORMAT = 4
+    FILE_NOT_FOUND = 5
+    EOF = 6
+    ERROR = 7
+
+    def is_ok(self) -> bool:
+        return self == PatchResult.OK or self == PatchResult.HUNK_SUCCEED
+
+
+PATCH_ERROR_REASONS = {
+    PatchResult.REVERSE_APPLIED: 'Already applied',
+    PatchResult.HUNK_FAILED: 'Hunk failed',
+    PatchResult.INVALID_FORMAT: 'Invalid format',
+    PatchResult.FILE_NOT_FOUND: 'Can\'t find file to patch',
+    PatchResult.EOF: 'Unexpected end of patch',
+    PatchResult.ERROR: 'Error',
+}
+
+
+def _diff(log: Logger, a: str, b: str, extra_args: list[str]) -> Optional[str]:
+    cmd = ['diff', '-updrN', *extra_args, a, b]
+    log(' '.join([cmd[0].upper()] + cmd[1:]))
+    p = sp.Popen(cmd,
+                 stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+    stdout, stderr = p.communicate()
+    log(f'stdout: {stdout.strip()}')
+    log(f'stderr: {stderr.strip()}')
+    return stdout if p.returncode != 2 else None
+
+
+def _patch(log: Logger, target: str, patch: str, extra_args: list[str]) -> PatchResult:
+    cmd = ['patch', '-p1', '-F0', *extra_args, target]
+    log(' '.join([cmd[0].upper()] + cmd[1:]))
+    p = sp.Popen(cmd,
+                 stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+    stdout, stderr = '', ''
+    with open(patch, 'rb') as f:
+        stdout, stderr = p.communicate(input=f.read())
+        stdout, stderr = stdout.decode(), stderr.decode()
+    log(f'stdout: {stdout.strip()}')
+    log(f'stderr: {stderr.strip()}')
+    if p.returncode == 0:
+        if re.search('Hunk\s*#\d+\s*succeeded', stdout) is not None:
+            return PatchResult.HUNK_SUCCEED
+        return PatchResult.OK
+
+    if 'patch: ****' in stderr:
+        is_gibberish = 'Only garbage was found in the patch input.' in stderr
+        is_malformed = 'malformed patch at' in stderr
+        if is_gibberish or is_malformed:
+            return PatchResult.INVALID_FORMAT
+        elif 'unexpected end of file in patch' in stderr:
+            return PatchResult.EOF
+        return PatchResult.ERROR
+
+    is_partly_succeeded = re.search('(patching\s*.*\n(Hunk\s*#\d+\s*succeeded.*\n)?){2,}', stdout) is not None
+    is_hunk_failed = re.search('Hunk\s*#\d+\s*FAILED', stdout) is not None
+    if 'Assume -R' in stdout:
+        return PatchResult.HUNK_FAILED if is_partly_succeeded else PatchResult.REVERSE_APPLIED
+    elif 'can\'t find file to patch' in stdout:
+        return PatchResult.HUNK_FAILED if is_partly_succeeded else PatchResult.FILE_NOT_FOUND
+    return PatchResult.HUNK_FAILED if is_hunk_failed or is_partly_succeeded else PatchResult.ERROR
+
+def _revert(log: Logger, target: str, patch: str, extra_args: list[str]) -> PatchResult:
+    return _patch(log, target, patch, ['-R', *extra_args])
