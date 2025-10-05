4b514bc5eda7e13fd9b2dd1d0fed8a02a4161bcd
come_ill_foo <rayveleaveme34@inbox.ru>
2025-10-05 21:38:39 +0100

    fix(ci): remove obsolete BS; feat: introduce commands for main

--- main.rkt
+++ main.rkt
@@ -24,7 +24,42 @@
 ;; http://docs.racket-lang.org/style/index.html
 
 ;; Code here
+(define EINVAL 22)
 
+(module+ main
+  (require racket/cmdline racket/match)
+  (define (do-init args)
+    (displayln "initializing current project as storage for patches...")
+    0)
+  (define (do-branch args)
+    (displayln "arranging a new patchset for version...")
+    0)
+  (define (do-apply args)
+    (displayln "applying patches...")
+    0)
+  (define (do-dehunk args)
+    (displayln "eliminating minor hunks discrepancies to patches...")
+    0)
+  (define (do-revert args)
+    (displayln "reverting patches...")
+    0)
+  (define (do-help args)
+    (displayln "Usage: patchiman command args")
+    EINVAL)
+  (define args-parse
+    (command-line
+      #:program "patchiman"
+      #:args (cmd . rest)
+      (exit
+        (match cmd
+          ["init" (do-init rest)]
+          ["branch" (do-branch rest)]
+          ["apply" (do-apply rest)]
+          ["dehunk" (do-dehunk rest)]
+          ["revert" (do-revert rest)]
+          ["help" (do-help rest)]
+          [_ (printf "unknown command: ~s\n" cmd)
+             (do-help rest)])))))
 
 
 (module+ test
@@ -39,12 +74,4 @@
   ;; this file is run using DrRacket or the `racket` executable.  The code here
   ;; does not run when this file is required by another module. Documentation:
   ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
+  (args-parse))
